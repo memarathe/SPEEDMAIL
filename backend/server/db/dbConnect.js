9d802f9863d748c1c1e4e0996efd2a4a3dab0541
Ruturaj Marathe <63833537+memarathe@users.noreply.github.com>
2024-01-17 10:56:24 -0500

    Update dbConnect.js

--- backend/server/db/dbConnect.js
+++ backend/server/db/dbConnect.js
@@ -6,7 +6,7 @@ async function dbConnect() {
   // use mongoose to connect this app to our database on mongoDB using the DB_URL (connection string)
   mongoose
     .connect(
-      "mongodb+srv://kaushik:1234@cluster0.9xdispc.mongodb.net/testDB2?retryWrites=true&w=majority",//"mongodb+srv://team23:team23@team23.57jtre7.mongodb.net/Speedmail?retryWrites=true&w=majority",// process.env.DB_URL,
+      "REPLACE WITH CONNECTION URL FOR MONGO DB",
       {
         //   these are options to ensure that the connection is done properly
         useNewUrlParser: true,
